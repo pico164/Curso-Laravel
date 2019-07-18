f3e8653e07f6fc6a3ee11d1c47dc2d377b8fdb21
luis.pico <lpicoleon@misena.edu.co>
2019-07-18 09:18:41 -0500

    Trabajo con seeders y models para recopilacion de data automatica

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use phpDocumentor\Reflection\Types\This;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncateTablas(['rol','permiso']);
         // $this->call(UsersTableSeeder::class);
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
+    }
+    protected function truncateTablas(array $tablas){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach ($tablas as $tabla) {
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
+
